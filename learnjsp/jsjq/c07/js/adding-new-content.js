01c4fb2adfebd2fa6ab44cd49df2c629146b78bb
Randy Davis <randy_davis@outlook.com>
2014-12-19 21:42:22 -0600

    Complete another attempt on ch 7 adding-new-content

--- learnjsp/jsjq/c07/js/adding-new-content.js
+++ learnjsp/jsjq/c07/js/adding-new-content.js
@@ -6,6 +6,14 @@
 // });
 
 // Another attempt
+// $(function() {
+//   $('ul').before('<p class="notice">Just updated</p>');
+//   $('li.hot').prepend('+ ');
+//   var $newListItem = $('<li><em>gluten-free</em> soy sauce</li>');
+//   $('li:last').after($newListItem);
+// });
+
+// Another attempt
 $(function() {
   $('ul').before('<p class="notice">Just updated</p>');
   $('li.hot').prepend('+ ');
