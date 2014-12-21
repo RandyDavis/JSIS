065e9ed16c4a592820ebca808733d73820541a34
Randy Davis <randy_davis@outlook.com>
2014-12-20 21:33:47 -0600

    complete ch 7 each method exercise

--- learnjsp/jsjq/c07/js/each.js
+++ learnjsp/jsjq/c07/js/each.js
@@ -1,5 +1,21 @@
+// $(function() {
+//    $('li').each(function() {
+//     var ids = this.id;
+//     $(this).append(' <span class="order">' + ids + '</span>');
+//   });
+// });
+
+// Another attempt
+// $(function() {
+//   $('li').each(function() {
+//     var ids = this.id;
+//     $(this).append(' <span class="order">' + ids + '</span>');
+//   });
+// });
+
+// Another attempt
 $(function() {
-   $('li').each(function() {
+  $('li').each(function() {
     var ids = this.id;
     $(this).append(' <span class="order">' + ids + '</span>');
   });
