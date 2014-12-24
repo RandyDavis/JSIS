a22300859c9092a26fc7c9ad1bfba715e4049aba
Randy Davis <randy_davis@outlook.com>
2014-12-23 22:21:44 -0600

    finish ch 7 event lesson practice

--- learnjsp/jsjq/c07/js/events.js
+++ learnjsp/jsjq/c07/js/events.js
@@ -1,3 +1,38 @@
+// $(function() {
+//   var ids = '';
+//   var $listItems = $('li');
+
+//   $listItems.on('mouseover click', function() {
+//     ids = this.id;
+//     $listItems.children('span').remove();
+//     $(this).append(' <span class="priority">' + ids + '</span>');
+//   });
+
+//   $listItems.on('mouseout', function() {
+//     $(this).children('span').remove();
+//   });
+
+// });
+
+
+// Another attempt
+// $(function() {
+//   var ids = '';
+//   var $listItems = $('li');
+
+//   $listItems.on('mouseover click', function() {
+//     ids = this.id;
+//     $listItems.children('span').remove();
+//     $(this).append(' <span class="priority">' + ids + '</span>');
+//   });
+
+//   $listItems.on('mouseout', function() {
+//     $(this).children('span').remove();
+//   });
+// });
+
+
+// Another attempt
 $(function() {
   var ids = '';
   var $listItems = $('li');
@@ -11,5 +46,4 @@ $(function() {
   $listItems.on('mouseout', function() {
     $(this).children('span').remove();
   });
-
 });
