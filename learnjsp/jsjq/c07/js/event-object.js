2c823e9dbedcc77a6fe550a657955945bb72a86b
Randy Davis <randy_davis@outlook.com>
2014-12-24 00:54:52 -0600

    complete ch 7 event object lesson

--- learnjsp/jsjq/c07/js/event-object.js
+++ learnjsp/jsjq/c07/js/event-object.js
@@ -1,5 +1,17 @@
-$(function() {
+// $(function() {
 
+//   $('li').on('click', function(e) {
+//     $('li span').remove();
+//     var date = new Date();
+//     date.setTime(e.timeStamp);
+//     var clicked = date.toDateString();
+//     $(this).append('<span class="date">' + clicked + ' ' + e.type + '</span>');
+//   });
+
+// });
+
+// Another attempt
+$(function() {
   $('li').on('click', function(e) {
     $('li span').remove();
     var date = new Date();
@@ -7,5 +19,4 @@ $(function() {
     var clicked = date.toDateString();
     $(this).append('<span class="date">' + clicked + ' ' + e.type + '</span>');
   });
-
 });
