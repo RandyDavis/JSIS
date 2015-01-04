867b6b0e92c0de354d01cc2b4760b2dd160874bd
Randy Davis <randy_davis@outlook.com>
2015-01-04 00:00:48 -0600

    one more time messing with effects

--- learnjsp/jsjq/c07/js/effects.js
+++ learnjsp/jsjq/c07/js/effects.js
@@ -14,6 +14,19 @@
 
 
 // Another attempt
+// $(function() {
+//   $('h2').hide().slideDown();
+//   var $li = $('li');
+//   $li.hide().each(function(index) {
+//     $(this).delay(700 * index).fadeIn(700);
+//   });
+//   $li.on('click', function() {
+//     $(this).fadeOut(700);
+//   });
+// });
+
+
+// Another attempt
 $(function() {
   $('h2').hide().slideDown();
   var $li = $('li');
