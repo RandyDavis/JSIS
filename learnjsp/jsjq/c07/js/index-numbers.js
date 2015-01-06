b72992442c97847812aea1d452bf5eea76b556c5
Randy Davis <randy_davis@outlook.com>
2015-01-05 23:38:23 -0600

    finish jQuery index-numbers lessons

--- learnjsp/jsjq/c07/js/index-numbers.js
+++ learnjsp/jsjq/c07/js/index-numbers.js
@@ -1,5 +1,21 @@
+// $(function() {
+//   $('li:lt(2)').removeClass('hot');
+//   $('li').eq(0).addClass('complete');
+//   $('li:gt(2)').addClass('cool');
+// });
+
+
+// Another attempt
+// $(function() {
+//   $('li:lt(2)').removeClass('hot');
+//   $('li:eq(0)').addClass('complete');
+//   $('li:gt(2)').addClass('cool');
+// });
+
+
+// Another attempt
 $(function() {
   $('li:lt(2)').removeClass('hot');
-  $('li').eq(0).addClass('complete');
+  $('li:eq(0)').addClass('complete');
   $('li:gt(2)').addClass('cool');
 });
