2bed5f3404183cae27da6c49e16a0b78a4fef7a4
Randy Davis <randy_davis@outlook.com>
2015-01-05 23:29:11 -0600

    finish jQuery filters lesson exercise

--- learnjsp/jsjq/c07/js/filters.js
+++ learnjsp/jsjq/c07/js/filters.js
@@ -1,3 +1,22 @@
+// $(function() {
+//   var $listItems = $('li');
+
+//   $listItems.filter('.hot:last').removeClass('hot');
+//   $('li:not(.hot)').addClass('cool');
+//   $listItems.has('em').addClass('complete');
+
+//   $listItems.each( function() {
+//     var $this = $(this);
+//     if ($this.is('.hot')) {
+//       $this.prepend('Priority item: ');
+//     }
+//   });
+
+//   $('li:contains("honey")').append(' (local)');
+// });
+
+
+// Another attempt
 $(function() {
   var $listItems = $('li');
 
@@ -5,7 +24,7 @@ $(function() {
   $('li:not(.hot)').addClass('cool');
   $listItems.has('em').addClass('complete');
 
-  $listItems.each( function() {
+  $listItems.each(function() {
     var $this = $(this);
     if ($this.is('.hot')) {
       $this.prepend('Priority item: ');
