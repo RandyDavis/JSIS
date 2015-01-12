cce3d2213b05ccc9fca8d3230ec39cb0b5523f6e
Randy Davis <randy_davis@outlook.com>
2015-01-11 23:50:16 -0600

    complete jquery positioning lesson as well as end of chapter major example

--- learnjsp/jsjq/c07/js/position.js
+++ learnjsp/jsjq/c07/js/position.js
@@ -1,16 +1,32 @@
+// $(function() {
+//   var $window = $(window);
+//   var $slideAd = $('#slideAd');
+//   var endZone = $('#footer').offset().top - $window.height() - 500;
+
+//   $window.on('scroll', function(){
+
+//     if  ( (endZone) < $window.scrollTop() ) {
+//       $slideAd.animate({ 'right': '0px' }, 250);
+//     } else {
+//       $slideAd.stop(true).animate({ 'right': '-360px' }, 250);
+//     }
+
+//   });
+
+// });
+
+
+// Another attempt
 $(function() {
   var $window = $(window);
   var $slideAd = $('#slideAd');
-  var endZone = $('#footer').offset().top - $window.height() - 500;
+  var endzone = $('#footer').offset().top - $window.height() - 500;
 
-  $window.on('scroll', function(){
-
-    if  ( (endZone) < $window.scrollTop() ) {
-      $slideAd.animate({ 'right': '0px' }, 250);
+  $window.on('scroll', function() {
+    if((endzone) < $window.scrollTop()) {
+      $slideAd.animate({'right': '0px'}, 250);
     } else {
-      $slideAd.stop(true).animate({ 'right': '-360px' }, 250);
+      $slideAd.stop(true).animate({'right': '-360px'}, 250);
     }
-
   });
-
 });
