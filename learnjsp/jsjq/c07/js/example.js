6aa26ced3accc250358619f3dafe86730f8ad049
Randy Davis <randy_davis@outlook.com>
2015-01-12 23:51:37 -0600

    add another try on jquery eoc example; finish data-html ajax exercise

--- learnjsp/jsjq/c07/js/example.js
+++ learnjsp/jsjq/c07/js/example.js
@@ -61,10 +61,71 @@
 
 
 // Another attempt
-$(function() {
+// $(function() {
 
+//   // SETUP
+//   var $list, $newItemForm, $newItemButton;
+//   var item = '';
+//   $list = $('ul');
+//   $newItemForm = $('#newItemForm');
+//   $newItemButton = $('#newItemButton');
+
+//   $('li').hide().each(function(index) {
+//     $(this).delay(450 * index).fadeIn(1600);
+//   });
+
+//   // ITEM Counter
+//   function updateCount() {
+//     var items = $('li[class!=complete]').length;
+//     $('#counter').text(items);
+//   }
+//   updateCount();
+
+//   // SETUP FORM FOR NEW ITEMS
+//   $newItemButton.show();
+//   $newItemForm.hide();
+//   $('#showForm').on('click', function() {
+//     $newItemButton.hide();
+//     $newItemForm.show();
+//   });
+
+//   // ADDING A NEW LIST ITEM
+//   $newItemForm.on('submit', function(e) {
+//     e.preventDefault();
+//     var text = $('input:text').val();
+//     $list.append('<li>' + text + '</li>');
+//     $('input:text').val('');
+//     updateCount();
+//   });
+
+//   // CLICK HANDLING - USES DELEGATION ON <ul> ELEMENT
+//   $list.on('click', 'li', function() {
+//     var $this = $(this);
+//     var complete = $this.hasClass('complete');
+
+//     if (complete === true) {
+//       $this.animate({
+//         opacity: 0.0,
+//         paddingLeft: '+180'
+//       }, 500, 'swing', function() {
+//         $this.remove();
+//       });
+//     } else {
+//       item = $this.text();
+//       $this.remove();
+//       $list
+//         .append('<li class=\"complete\">' + item + '</li>')
+//         .hide().fadeIn(300);
+//       updateCount();
+//     }
+//   });
+// });
+
+
+// Another attempt
+$(function() {
   // SETUP
-  var $list, $newItemForm, $newItemButton;
+  var $list, $newItemButton, $newItemForm;
   var item = '';
   $list = $('ul');
   $newItemForm = $('#newItemForm');
@@ -119,6 +180,8 @@ $(function() {
       updateCount();
     }
   });
+
+
 });
 
 
--- learnjsp/jsjq/c08/js/data-html.js
+++ learnjsp/jsjq/c08/js/data-html.js
@@ -1,16 +1,29 @@
-var xhr = new XMLHttpRequest();                 // Create XMLHttpRequest object
+// var xhr = new XMLHttpRequest();                 // Create XMLHttpRequest object
 
-xhr.onload = function() {                       // When response has loaded
-  // The following conditional check will not work locally - only on a server
-  if(xhr.status === 200) {                       // If server status was ok
-    document.getElementById('content').innerHTML = xhr.responseText; // Update
+// xhr.onload = function() {                       // When response has loaded
+//   // The following conditional check will not work locally - only on a server
+//   if(xhr.status === 200) {                       // If server status was ok
+//     document.getElementById('content').innerHTML = xhr.responseText; // Update
+//   }
+// };
+
+// xhr.open('GET', 'data/data.html', true);        // Prepare the request
+// xhr.send(null);                                 // Send the request
+
+// // NOTE: If you run this file locally
+// // You will not get a server status
+// // You can set the conditional statement to true on line 5 as shown below
+// // if(true) {
+
+
+// Another Attempt
+var xhr = new XMLHttpRequest();
+
+xhr.onload = function() {
+  if (xhr.status === 200) {
+    document.getElementById('content').innerHTML = xhr.responseText;
   }
 };
 
-xhr.open('GET', 'data/data.html', true);        // Prepare the request
-xhr.send(null);                                 // Send the request
-
-// NOTE: If you run this file locally
-// You will not get a server status
-// You can set the conditional statement to true on line 5 as shown below
-// if(true) {
+xhr.open('GET', 'data/data.html', true);
+xhr.send(null);
