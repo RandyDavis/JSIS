5ed071f12d4e115b99eeada552a9b6757458321a
Randy Davis <randy_davis@outlook.com>
2015-01-13 17:48:35 -0600

    finish jsonp example for ajax lesson

--- learnjsp/jsjq/c08/js/data-jsonp.js
+++ learnjsp/jsjq/c08/js/data-jsonp.js
@@ -1,16 +1,35 @@
-function showEvents(data) {                           // Callback when JSON loads
-  var newContent = '';                                // Variable to hold HTML
+// function showEvents(data) {                           // Callback when JSON loads
+//   var newContent = '';                                // Variable to hold HTML
  
-    // BUILD UP STRING WITH NEW CONTENT (could also use DOM manipulation)
-    for (var i = 0; i < data.events.length; i++) {    // Loop through object
-      newContent += '<div class="event">';
-      newContent += '<img src="' + data.events[i].map + '" ';
-      newContent += 'alt="' + data.events[i].location + '" />';
-      newContent += '<p><b>' + data.events[i].location + '</b><br>';
-      newContent += data.events[i].date + '</p>';
-      newContent += '</div>';
-    }
+//     // BUILD UP STRING WITH NEW CONTENT (could also use DOM manipulation)
+//     for (var i = 0; i < data.events.length; i++) {    // Loop through object
+//       newContent += '<div class="event">';
+//       newContent += '<img src="' + data.events[i].map + '" ';
+//       newContent += 'alt="' + data.events[i].location + '" />';
+//       newContent += '<p><b>' + data.events[i].location + '</b><br>';
+//       newContent += data.events[i].date + '</p>';
+//       newContent += '</div>';
+//     }
 
-    // Update the page with the new content
-    document.getElementById('content').innerHTML = newContent;
+//     // Update the page with the new content
+//     document.getElementById('content').innerHTML = newContent;
+// }
+
+
+// Another attempt
+function showEvents(data) {
+  var newContent = '';
+
+  // BUILD UP STRING WITH NEW CONTENT (could also use DOM manipulation)
+  for (var i = 0; i < data.events.length; i++) {
+    newContent += '<div class="event">';
+    newContent += '<img src="' + data.events[i].map + '" ';
+    newContent += 'alt="' + data.events[i].location + '" />';
+    newContent += '<p><b>' + data.events[i].location + '</b><br>';
+    newContent += data.events[i].date + '</p>';
+    newContent += '</div>';
+  }
+
+  // Update the page with the new content
+  document.getElementById('content').innerHTML = newContent;
 }
