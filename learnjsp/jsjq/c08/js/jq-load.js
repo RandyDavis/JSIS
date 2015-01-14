ee150989b8c0007d100756d01102fd349a260df2
Randy Davis <randy_davis@outlook.com>
2015-01-14 01:04:07 -0600

    finish loading html w/jquery lesson

--- learnjsp/jsjq/c08/js/jq-load.js
+++ learnjsp/jsjq/c08/js/jq-load.js
@@ -1,10 +1,23 @@
-$('nav a').on('click', function(e) {                 // User clicks nav link
-  e.preventDefault();                                // Stop loading new link
-  var url = this.href;                               // Get value of href
+// $('nav a').on('click', function(e) {                 // User clicks nav link
+//   e.preventDefault();                                // Stop loading new link
+//   var url = this.href;                               // Get value of href
 
-  $('nav a.current').removeClass('current');         // Clear current indicator
-  $(this).addClass('current');                       // New current indicator
+//   $('nav a.current').removeClass('current');         // Clear current indicator
+//   $(this).addClass('current');                       // New current indicator
 
-  $('#container').remove();                          // Remove old content
-  $('#content').load(url + ' #content').hide().fadeIn('slow'); // New content
+//   $('#container').remove();                          // Remove old content
+//   $('#content').load(url + ' #content').hide().fadeIn('slow'); // New content
+// });
+
+
+// Another attempt
+$('nav a').on('click', function(e) {
+  e.preventDefault();
+  var url = this.href;
+
+  $('nav a.current').removeClass('current');
+  $(this).addClass('current');
+
+  $('#container').remove();
+  $('#content').load(url + ' #content').hide().fadeIn('slow');
 });
