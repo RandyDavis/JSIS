20513be38ca325a56605daa7ca016ea00d808c6f
Randy Davis <randy_davis@outlook.com>
2015-02-04 00:27:40 -0600

    add adding and removing models section of exercise

--- learnbb/devbbjs-book/ch3/collections.js
+++ learnbb/devbbjs-book/ch3/collections.js
@@ -1,3 +1,25 @@
+// *** COLLECTIONS ***
+// var Todo = Backbone.Model.extend({
+//   defaults: {
+//     title: '',
+//     completed: false
+//   }
+// });
+
+// var TodoCollection = Backbone.Collection.extend({
+//   model: Todo
+// });
+
+// var myTodo = new TodoCollection({ title: 'Read the whole book', id: 2 });
+
+// // pass array of models on collection instantiation
+// var todos = new TodoCollection([myTodo]);
+// console.log("Collection size: " + todos.length);  // Collection size: 1
+
+
+
+
+// *** Adding and Removing Models ***
 var Todo = Backbone.Model.extend({
   defaults: {
     title: '',
@@ -5,12 +27,60 @@ var Todo = Backbone.Model.extend({
   }
 });
 
-var TodoCollection = Backbone.Collection.extend({
+var TodosCollection = Backbone.Collection.extend({
   model: Todo
 });
 
-var myTodo = new TodoCollection({ title: 'Read the whole book', id: 2 });
+var a = new Todo({ title: "Go to Jamaica." }),
+    b = new Todo({ title: "Go to China." }),
+    c = new Todo({ title: "Go to Disneyland." });
 
-// pass array of models on collection instantiation
-var todos = new TodoCollection([myTodo]);
-console.log("Collection size: " + todos.length);  // Collection size: 1
+var todos = new TodosCollection([a,b]);
+console.log("Collection size: " + todos.length); // Logs: Collection size: 2
+
+todos.add(c);
+console.log("Collection size: " + todos.length); // Logs: Collection size: 3
+
+todos.remove([a,b]);
+console.log("Collection size: " + todos.length); // Logs: Collection size: 1
+
+todos.remove(c);
+console.log("Collection size: " + todos.length); // Logs: Collection size: 0
+
+todos.add([a,b,c]);
+console.log("Collection size: " + todos.length); // Logs: Collection size: 3
+
+todos.remove(a);
+console.log("Collection size: " + todos.length); // Logs: Collection size: 2
+
+todos.remove(a);
+console.log("Collection size: " + todos.length); // Still Logs: Collection size: 2
+
+
+// Also note that when you are using add() on a collection, 
+// passing {merge: true} causes duplicate models to have their 
+// attributes merged into the existing models, instead of being ignored.
+var items = new Backbone.Collection;
+items.add([{ id: 1, name: "Dog", age: 3 }, { id: 2, name: "cat", age: 2 }]);
+items.add([{ id: 1, name: "Bear"}], {merge: true });
+items.add([{ id: 2, name: "lion" }]); // merge: false
+
+console.log(JSON.stringify(items.toJSON()));
+// "[{"id":1,"name":"Bear","age":3},{"id":2,"name":"cat","age":2}]"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
