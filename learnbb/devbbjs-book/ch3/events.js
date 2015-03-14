5e2f95afdc750f8d9a56ad43635f2f2eca707a29
Randy Davis <randy_davis@outlook.com>
2015-03-13 23:16:37 -0500

    finish remaining parts of triggers of events lesson

--- learnbb/devbbjs-book/ch3/events.js
+++ learnbb/devbbjs-book/ch3/events.js
@@ -67,41 +67,109 @@
 
 
 
-// *** Off Event: removes callback functions that were preoviously bound to an object. ***
+// // *** Off Event: removes callback functions that were preoviously bound to an object. ***
+// var ourObject = {};
+
+// // Mixin
+// _.extend(ourObject, Backbone.Events);
+
+// function dancing(msg) {
+//   console.log("We " + msg);
+// }
+
+// // Add namespaced custom events
+// ourObject.on("dance:tap", dancing);
+// ourObject.on("dance:break", dancing);
+
+// // Trigger the custom events.  Each will be caught and acted upon.
+// ourObject.trigger("dance:tap", "started tap dancing! Yeah!");
+// ourObject.trigger("dance:break", "started break dancing! Yeah!");
+
+// // Removes event bound to the object
+// ourObject.off("dance:tap");
+
+// // Trigger custome events again, but one is logged
+// ourObject.trigger("dance:tap", "stopped tap dancing.");
+
+// // won't e logged as it's not listend for
+// ourObject.trigger("dance:break", "break dancing. Yeah!");
+
+
+
+
+
+// *** Remove ALL Callbacks ***
+// var ourObject = {};
+
+// // Mixin
+// _.extend(ourObject, Backbone.Events);
+
+// function dancing(msg) {
+//   console.log("We are dancing " + msg);
+// }
+
+// function jumping(msg) {
+//   console.log("We are jumping " + msg);
+// }
+
+// // Add two listeners to the same event
+// ourObject.on("move", dancing);
+// ourObject.on("movoe", dancing);
+
+// // Trigger the events.  Bothe listeners are called
+// ourObject.trigger("move", "Yeah!");
+
+// // Remove specified listener
+// ourObject.off("move", dancing);
+
+// // Trigger the events again.  One listener left
+// ourObject.trigger("move", "Yeah, jump, jump!");
+
+
+
+
+
+// *** Triggering Single and Multiple Events ***
+// var ourObject = {};
+
+// // Mixin
+// _.extend(ourObject, Backbone.Events);
+
+// function doAction(msg) { console.log("We are " + msg);}
+
+// // Add listeners
+// ourObject.on("dance", doAction);
+// ourObject.on("jump", doAction);
+// ourObject.on("skip", doAction);
+
+// // Single Event
+// ourObject.trigger("dance", "just dancing");
+
+// // Multiple Events
+// ourObject.trigger("dance jump skip", 'vary tired from so much action!');
+
+
+
+// *** Trigger Passing Multiple Args to Callback Function ***
 var ourObject = {};
 
 // Mixin
 _.extend(ourObject, Backbone.Events);
 
-function dancing(msg) {
-  console.log("We " + msg);
+function doAction(action, duration) {
+  console.log("We are " + action + ' for ' + duration);
 }
 
-// Add namespaced custom events
-ourObject.on("dance:tap", dancing);
-ourObject.on("dance:break", dancing);
+// Add Listeners
+ourObject.on("dance", doAction);
+ourObject.on("jump", doAction);
+ourObject.on("skip", doAction);
 
-// Trigger the custom events.  Each will be caught and acted upon.
-ourObject.trigger("dance:tap", "started tap dancing! Yeah!");
-ourObject.trigger("dance:break", "started break dancing! Yeah!");
+// Passing multiple arguments to single event
+ourObject.trigger("dance", 'dancing', "5 minutes");
 
-// Removes event bound to the object
-ourObject.off("dance:tap");
-
-// Trigger custome events again, but one is logged
-ourObject.trigger("dance:tap", "stopped tap dancing.");
-
-// won't e logged as it's not listend for
-ourObject.trigger("dance:break", "break dancing. Yeah!");
-
-
-
-
-
-
-
-
-
+// Passing multiple arguments to multiple evnts
+ourObject.trigger("dance jump skip", 'on fire', "15 minutes");
 
 
 
