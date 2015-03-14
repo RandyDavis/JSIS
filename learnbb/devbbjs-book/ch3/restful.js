fae9a8d0b36f925ab513000bac5253303cdba0ba
Randy Davis <randy_davis@outlook.com>
2015-03-13 20:06:31 -0500

    add deleting models from the server lesson

--- learnbb/devbbjs-book/ch3/restful.js
+++ learnbb/devbbjs-book/ch3/restful.js
@@ -19,6 +19,32 @@
 
 
 // *** Saving Models to the Server ***
+// var Todo = Backbone.Model.extend({
+//   defaults: {
+//     title: '',
+//     completed: false
+//   }
+// });
+
+// var TodosCollection = Backbone.Collection.extend({
+//   model: Todo,
+//   url: '/todos'
+// });
+
+// var todos = new TodosCollection();
+// todos.fetch();
+
+// var todo2 = todos.get(2);
+// todo2.set('title', 'go fishing');
+// todo2.save(); // sends HTTP PUT to /todos/2
+
+// todos.create({ title: 'Try out code samples' });
+// // sends HTTP POST to /todos and adds to collection
+
+
+
+
+// *** Deleting Models from the Server ***
 var Todo = Backbone.Model.extend({
   defaults: {
     title: '',
@@ -35,8 +61,34 @@ var todos = new TodosCollection();
 todos.fetch();
 
 var todo2 = todos.get(2);
-todo2.set('title', 'go fishing');
-todo2.save(); // sends HTTP PUT to /todos/2
+todo2.destroy(); // sends HTTP DELETE to /todos/2 and removes from collection
 
-todos.create({ title: 'Try out code samples' });
-// sends HTTP POST to /todos and adds to collection
+// Calling destroy() on a model will return false if the model isNew:
+var Todo = new Backbone.Model();
+console.log(Todo.destroy()); // false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
