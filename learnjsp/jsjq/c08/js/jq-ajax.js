62515e9f0c2437b2c3697241ad1b3d54c1d6bfac
Randy Davis <randy_davis@outlook.com>
2015-04-27 23:50:51 -0500

    complete controlling ajax example

--- learnjsp/jsjq/c08/js/jq-ajax.js
+++ learnjsp/jsjq/c08/js/jq-ajax.js
@@ -1,28 +1,57 @@
+// $('nav a').on('click', function(e) {
+//   e.preventDefault();
+//   var url = this.href;                                      // URL to load
+//   var $content = $('#content');                             // Cache selection
+
+//   $('nav a.current').removeClass('current');                // Update links
+//   $(this).addClass('current');
+//   $('#container').remove();                                 // Remove content
+
+//   $.ajax({
+//     type: "POST",                                           // GET or POST
+//     url: url,                                               // Path to file
+//     timeout: 2000,                                          // Waiting time
+//     beforeSend: function() {                                // Before Ajax 
+//       $content.append('<div id="load">Loading</div>');      // Load message
+//     },
+//     complete: function() {                                  // Once finished
+//       $('#loading').remove();                               // Clear message
+//     },
+//     success: function(data) {                               // Show content
+//       $content.html( $(data).find('#container') ).hide().fadeIn(400);
+//     },
+//     fail: function() {                                      // Show error msg 
+//       $('#panel').html('<div class="loading">Please try again soon.</div>');
+//     }
+//   });
+
+// });
+
+
 $('nav a').on('click', function(e) {
   e.preventDefault();
-  var url = this.href;                                      // URL to load
-  var $content = $('#content');                             // Cache selection
+  var url = this.href;                        // URL to load
+  var $content = $('#content');               // Cache Selection
 
-  $('nav a.current').removeClass('current');                // Update links
+  $('nav a.current').removeClass('current');  // Update links
   $(this).addClass('current');
-  $('#container').remove();                                 // Remove content
+  $('#container').remove();                   // Remove content
 
   $.ajax({
-    type: "POST",                                           // GET or POST
-    url: url,                                               // Path to file
-    timeout: 2000,                                          // Waiting time
-    beforeSend: function() {                                // Before Ajax 
-      $content.append('<div id="load">Loading</div>');      // Load message
+    type: "GET",
+    url: url,
+    timeout: 2000,
+    beforeSend: function() {
+      $content.append('<div id="load">Loading</div>');
     },
-    complete: function() {                                  // Once finished
-      $('#loading').remove();                               // Clear message
+    complete: function() {
+      $('#loading').remove();
     },
-    success: function(data) {                               // Show content
-      $content.html( $(data).find('#container') ).hide().fadeIn(400);
+    success: function(data) {
+      $content.html($(data).find('#container')).hide().fadeIn(400);
     },
-    fail: function() {                                      // Show error msg 
+    fail: function() {
       $('#panel').html('<div class="loading">Please try again soon.</div>');
     }
   });
-
 });
