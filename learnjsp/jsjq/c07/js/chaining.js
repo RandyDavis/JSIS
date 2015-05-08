48aeb8236f885e7192f3e14821c536def15f28e6
Randy Davis <randy_davis@outlook.com>
2015-05-07 23:35:52 -0500

    finished chaining example

--- learnjsp/jsjq/c07/js/chaining.js
+++ learnjsp/jsjq/c07/js/chaining.js
@@ -1,3 +1,8 @@
-$('li[id!="one"]').hide().delay(500).fadeIn(1400); // semi-colon indicates end of chaining - can be writen on separate lines
+// $('li[id!="one"]').hide().delay(500).fadeIn(1400); // semi-colon indicates end of chaining - can be writen on separate lines
+// $('li:first-child').addClass('next');
+// $('li.priority').addClass('highlight');
+
+// Attempt 1
+$('li[id!="one"]').hide().delay(500).fadeIn(1500);
 $('li:first-child').addClass('next');
 $('li.priority').addClass('highlight');
