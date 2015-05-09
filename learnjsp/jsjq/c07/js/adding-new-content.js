e6b4c5c4f8bf2d9e02527a9a62dc91d2eddca18b
Randy Davis <randy_davis@outlook.com>
2015-05-09 00:07:25 -0500

    finished adding new content section

--- learnjsp/jsjq/c07/js/adding-new-content.js
+++ learnjsp/jsjq/c07/js/adding-new-content.js
@@ -14,8 +14,16 @@
 // });
 
 // Another attempt
+// $(function() {
+//   $('ul').before('<p class="notice">Just updated</p>');
+//   $('li.hot').prepend('+ ');
+//   var $newListItem = $('<li><em>gluten-free</em> soy sauce</li>');
+//   $('li:last').after($newListItem);
+// });
+
+// Another attempt
 $(function() {
-  $('ul').before('<p class="notice">Just updated</p>');
+  $('ul').before('<p class="notice"> Just updated</p>');
   $('li.hot').prepend('+ ');
   var $newListItem = $('<li><em>gluten-free</em> soy sauce</li>');
   $('li:last').after($newListItem);
