8cca8286b75eb4cc99acc5960c3b53c00cf3dfbd
Randy Davis <randy_davis@outlook.com>
2015-05-09 23:54:04 -0500

    finish another attempt on each method in jQuery

--- learnjsp/jsjq/c07/js/each.js
+++ learnjsp/jsjq/c07/js/each.js
@@ -14,6 +14,14 @@
 // });
 
 // Another attempt
+// $(function() {
+//   $('li').each(function() {
+//     var ids = this.id;
+//     $(this).append(' <span class="order">' + ids + '</span>');
+//   });
+// });
+
+// Another attempt
 $(function() {
   $('li').each(function() {
     var ids = this.id;
