882cb48dde06d067fa689a77b2fba36b5ce009ca
Randy Davis <randy_davis@outlook.com>
2015-05-12 23:51:17 -0500

    finish basic effects quick example

--- learnjsp/jsjq/c07/js/effects.js
+++ learnjsp/jsjq/c07/js/effects.js
@@ -27,13 +27,28 @@
 
 
 // Another attempt
+// $(function() {
+//   $('h2').hide().slideDown();
+//   var $li = $('li');
+//   $li.hide().each(function(index) {
+//     $(this).delay(700 * index).fadeIn(700);
+//   });
+//   $li.on('click', function() {
+//     $(this).fadeOut(700);
+//   });
+// });
+
+
+// Another attempt
 $(function() {
   $('h2').hide().slideDown();
   var $li = $('li');
+
   $li.hide().each(function(index) {
     $(this).delay(700 * index).fadeIn(700);
   });
+
   $li.on('click', function() {
-    $(this).fadeOut(700);
+    $(this).fadeOut();
   });
-});
+}); 
