4976b0f84a663a1eeb4799765006213018b8e2eb
Randy Davis <randy_davis@outlook.com>
2015-09-14 22:52:17 -0500

    finish event redos

--- learnjsp/jsjq/c07/js/event-object.js
+++ learnjsp/jsjq/c07/js/event-object.js
@@ -24,6 +24,22 @@
 
 
 // Another attempt
+//$(function() {
+//  $('li').on('click mouseover', function(e) {
+//    $('li span').remove();
+//    var date = new Date();
+//    date.setTime(e.timeStamp);
+//    var clicked = date.toDateString();
+//    $(this).append('<span class="date">' + clicked + ' ' + e.type + '</span>');
+//  });
+//
+//  $('li').on('mouseout', function() {
+//    $('li span').remove();
+//  });
+//});
+
+
+// Another try
 $(function() {
   $('li').on('click mouseover', function(e) {
     $('li span').remove();
