4fd9082645ac21ec5fd3ff0f2d29ac469c4879d6
Randy Davis <randy_davis@outlook.com>
2015-09-16 23:58:54 -0500

    redo event delegation again

--- learnjsp/jsjq/c07/js/event-delegation.js
+++ learnjsp/jsjq/c07/js/event-delegation.js
@@ -70,20 +70,39 @@
 
 
 // Another try
-$(function() {
-  var listItem, itemStatus, eventType;
+//$(function() {
+//  var listItem, itemStatus, eventType;
+//
+//  $('ul').on(
+//      'click mouseover',
+//      ':not(#four)',
+//      {status: 'important'},
+//      function(e) {
+//        listItem = 'Item: ' + e.target.textContent + '<br>';
+//        itemStatus = 'Status: ' + e.data.status + '<br>';
+//        eventType = 'Event: ' + e.type;
+//        $('#notes').html(listItem + itemStatus + eventType);
+//      }
+//  );
+//});
+//
 
-  $('ul').on(
-      'click mouseover',
-      ':not(#four)',
-      {status: 'important'},
-      function(e) {
-        listItem = 'Item: ' + e.target.textContent + '<br>';
-        itemStatus = 'Status: ' + e.data.status + '<br>';
-        eventType = 'Event: ' + e.type;
-        $('#notes').html(listItem + itemStatus + eventType);
-      }
-  );
+
+// another try
+$(function() {
+    var listItem, itemStatus, eventType;
+
+    $('ul').on(
+        'click mouseover',
+        ':not(#four)',
+        {status: 'important'},
+        function(e) {
+            listItem = 'Item: ' + e.target.textContent + '<br>';
+            itemStatus = 'Status: ' + e.data.status + '<br>';
+            eventType = 'Event: ' + e.type;
+            $('#notes').html(listItem + itemStatus + eventType);
+        }
+    );
 });
 
 
